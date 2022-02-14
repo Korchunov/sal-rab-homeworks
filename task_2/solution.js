7fe4b45cd6adb5baa3a31cc6a1a5d926d7f9d2c7
Никита Корчунов <n.s.korchunov@gmail.com>
2022-02-14 16:48:12 +0400

    jhgfds

--- task_2/solution.js
+++ task_2/solution.js
@@ -3,12 +3,10 @@ function calcShipping(sum, min, shipping) {
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
     let shippingSum;
-    if (productsSum == 0) {
-        shippingSum = 0;
-    } else if (productsSum >= freeShippingMinSum) {
+    if ((productsSum == 0) || (productsSum >= freeShippingMinSum)) {
             shippingSum = 0;
         } else if ((productsSum > 0) & (productsSum < freeShippingMinSum)) { 
-            shippingSum == shipping;
+            shippingSum = shipping;
         } 
         
 
