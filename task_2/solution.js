c2c51ceab4865361bfa1ae14cc42480775ed142a
Никита Корчунов <n.s.korchunov@gmail.com>
2022-02-14 16:18:25 +0400

    попытка ещё одна

--- task_2/solution.js
+++ task_2/solution.js
@@ -10,7 +10,7 @@ function calcShipping(sum, min, shipping) {
         }
 
         else if (productsSum > 0 && productsSum < freeShippingMinSum) { 
-            shippingSum = shippingPrice;
+            shippingSum = shipping;
         } 
         
 
