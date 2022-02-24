96d0942437fce1b1f5925fb10670641340d6e0fd
Никита Корчунов <n.s.korchunov@gmail.com>
2022-02-24 13:36:48 +0400

    cut

--- task_4/solution.js
+++ task_4/solution.js
@@ -7,15 +7,13 @@
 
 function parseProducts(json) {
     let data = JSON.parse(json);
-    let products;
-    products = data.products;
+    let products = data.products;
     return parseProducts.products;
 }
 
 function renderProductsCards(json){
     clearProducts();
-    let products;
-    products = parseProducts(json);
+    let products = parseProducts(json);
     let length = products.length;
     for (let i = 0; i < length; i += 1) {
     addProduct(products[i]);
