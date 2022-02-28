3bf9a149033c8cc0db0791193243273009724335
Никита Корчунов <n.s.korchunov@gmail.com>
2022-02-28 11:04:17 +0400

    chetyri

--- task_final/solution.js
+++ task_final/solution.js
@@ -4,6 +4,7 @@ function sendRequest(name, phone, address, goods, sum) {
     data.client.phone = phone;
     data.client.name = name;
     data.order.sum = sum;
+    data.goods = goods;
     data.order.sum = name + phone + address + goods + sum;
 
     const received = JSON.parse(sendRequest(name, phone, address, goods, sum));
